fb7139bd65def0b77464ab5c281f0aac92298abb
Roberto Paraiso <roberto.paraiso@mngs.com.br>
2023-02-25 02:09:13 -0300

    criacao do projeto curso alura
    Signed-off-by: Roberto Paraiso <roberto.paraiso@mngs.com.br>

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+
+public record DadosListagemMedico(Long id, Boolean ativo, String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getId(), medico.getAtivo(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
